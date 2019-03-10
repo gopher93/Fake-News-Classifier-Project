ce6ac281f206362dcb291c29fb5803d5a774bccf
Kane Gillespie Pickrel <kpickrel@ball.com>
2019-03-10 14:17:01 -0400

    Add files via upload

---
+++ fakenews.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  9 15:44:51 2019
+
+@author: Kane
+
+TextClassification
+Fake News Identification
+"""
+
+# Import the necessary modules
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+path =  r'C:\fake_or_real_news.csv'
+df = pd.read_csv(path, encoding = 'utf-8')
+# Print the head of df
+print(df.head())
+
+# Create a series to store the labels: y
+y = df.label
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(df['text'], y, test_size=0.33, random_state=53)
+
+# Initialize a CountVectorizer object: count_vectorizer
+count_vectorizer = CountVectorizer(stop_words='english')
+
+# Transform the training data using only the 'text' column values: count_train 
+count_train = count_vectorizer.fit_transform(X_train)
+
+# Transform the test data using only the 'text' column values: count_test 
+count_test = count_vectorizer.transform(X_test)
+
+# Print the first 10 features of the count_vectorizer
+print(count_vectorizer.get_feature_names()[:10])
